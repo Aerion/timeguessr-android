f0742ea6eb75e0cc904b89046077e9940259b642
Guillaume Taquet Gasperini <perso@aerion.me>
2024-10-17 20:57:36 +0200

    ✨ Increase image zoom factor to 10x

--- app/src/main/java/me/aerion/timeguessr/PhotoPage.kt
+++ app/src/main/java/me/aerion/timeguessr/PhotoPage.kt
@@ -3,13 +3,21 @@ package me.aerion.timeguessr
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import me.saket.telephoto.zoomable.ZoomSpec
 import me.saket.telephoto.zoomable.coil.ZoomableAsyncImage
+import me.saket.telephoto.zoomable.rememberZoomableImageState
+import me.saket.telephoto.zoomable.rememberZoomableState
 
 @Composable
 fun PhotoPage(roundData: RoundData, modifier: Modifier = Modifier) {
+    val state = rememberZoomableImageState(rememberZoomableState(
+        zoomSpec = ZoomSpec(maxZoomFactor = 10f)
+    ))
+
     ZoomableAsyncImage(
         model = roundData.URL,
         contentDescription = null,
         modifier = modifier.fillMaxSize(),
+        state = state
     )
 }
