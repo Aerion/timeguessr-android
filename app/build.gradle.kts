f060ff28af75e274be41063b7f6abec0452f18fc
Guillaume Taquet Gasperini <perso@aerion.me>
2024-11-02 22:13:52 +0100

    🐛 Fix black photos for some colorspaces
    As per [0], hardcode the SRGB colorspace for now to prevent
    having black pictures on some images.
    
    [0] https://github.com/saket/telephoto/issues/110

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "me.aerion.timeguessr"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/me/aerion/timeguessr/PhotoPage.kt
+++ app/src/main/java/me/aerion/timeguessr/PhotoPage.kt
@@ -1,8 +1,11 @@
 package me.aerion.timeguessr
 
+import android.graphics.ColorSpace
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import coil.request.ImageRequest
 import me.saket.telephoto.zoomable.ZoomableImageState
 import me.saket.telephoto.zoomable.coil.ZoomableAsyncImage
 
@@ -12,9 +15,11 @@ fun PhotoPage(
     photoZoomState: ZoomableImageState,
     modifier: Modifier = Modifier
 ) {
-
     ZoomableAsyncImage(
-        model = roundData.URL,
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(roundData.URL)
+            .colorSpace(ColorSpace.get(ColorSpace.Named.SRGB))
+            .build(),
         contentDescription = null,
         modifier = modifier.fillMaxSize(),
         state = photoZoomState
